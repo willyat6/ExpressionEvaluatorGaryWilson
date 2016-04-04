f24563c945c65910fdcf301a9b355c1a56fbfe86
willyat6 <willyat6@hotmail.com>
2016-04-04 14:49:19 +0100

    Simple expression evaluator

---
+++ SimpleExpressionEvaluator/SimpleExpressionEvaluator/FormEvaluator.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SimpleExpressionEvaluator
+{
+    partial class FormEvaluator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxExpression = new System.Windows.Forms.TextBox();
+            this.btnEvaluate = new System.Windows.Forms.Button();
+            this.listBoxEvaluationResults = new System.Windows.Forms.ListBox();
+            this.labelExpression = new System.Windows.Forms.Label();
+            this.labelEvalResults = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxExpression
+            // 
+            this.textBoxExpression.Location = new System.Drawing.Point(35, 51);
+            this.textBoxExpression.Name = "textBoxExpression";
+            this.textBoxExpression.Size = new System.Drawing.Size(282, 20);
+            this.textBoxExpression.TabIndex = 0;
+            // 
+            // btnEvaluate
+            // 
+            this.btnEvaluate.Location = new System.Drawing.Point(335, 50);
+            this.btnEvaluate.Name = "btnEvaluate";
+            this.btnEvaluate.Size = new System.Drawing.Size(75, 23);
+            this.btnEvaluate.TabIndex = 1;
+            this.btnEvaluate.Text = "Evaluate";
+            this.btnEvaluate.UseVisualStyleBackColor = true;
+            this.btnEvaluate.Click += new System.EventHandler(this.btnEvaluate_Click);
+            // 
+            // listBoxEvaluationResults
+            // 
+            this.listBoxEvaluationResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxEvaluationResults.FormattingEnabled = true;
+            this.listBoxEvaluationResults.Location = new System.Drawing.Point(35, 107);
+            this.listBoxEvaluationResults.Name = "listBoxEvaluationResults";
+            this.listBoxEvaluationResults.Size = new System.Drawing.Size(376, 277);
+            this.listBoxEvaluationResults.TabIndex = 2;
+            // 
+            // labelExpression
+            // 
+            this.labelExpression.AutoSize = true;
+            this.labelExpression.Location = new System.Drawing.Point(32, 35);
+            this.labelExpression.Name = "labelExpression";
+            this.labelExpression.Size = new System.Drawing.Size(114, 13);
+            this.labelExpression.TabIndex = 3;
+            this.labelExpression.Text = "Expression to evaluate";
+            // 
+            // labelEvalResults
+            // 
+            this.labelEvalResults.AutoSize = true;
+            this.labelEvalResults.Location = new System.Drawing.Point(32, 91);
+            this.labelEvalResults.Name = "labelEvalResults";
+            this.labelEvalResults.Size = new System.Drawing.Size(42, 13);
+            this.labelEvalResults.TabIndex = 4;
+            this.labelEvalResults.Text = "Results";
+            // 
+            // FormEvaluator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 422);
+            this.Controls.Add(this.labelEvalResults);
+            this.Controls.Add(this.labelExpression);
+            this.Controls.Add(this.listBoxEvaluationResults);
+            this.Controls.Add(this.btnEvaluate);
+            this.Controls.Add(this.textBoxExpression);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormEvaluator";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Expression Evaluator - Gary Wilson";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxExpression;
+        private System.Windows.Forms.Button btnEvaluate;
+        private System.Windows.Forms.ListBox listBoxEvaluationResults;
+        private System.Windows.Forms.Label labelExpression;
+        private System.Windows.Forms.Label labelEvalResults;
+    }
+}
+
---
+++ SimpleExpressionEvaluator/SimpleExpressionEvaluator/FormEvaluator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace SimpleExpressionEvaluator
+{
+    public partial class FormEvaluator : Form
+    {
+        private const string NOExpression = "No expression was supplied";
+        private DataTable Evaluator = new DataTable();
+
+        public FormEvaluator()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateResults(string result)
+        {
+            listBoxEvaluationResults.Items.Add(result);
+        }
+
+        public string Evaluate(string expression)
+        {
+            string result = Evaluator.Compute(expression, string.Empty).ToString();
+            if (result == string.Empty)
+            {
+                result = "Failed to evaluate: " + expression;
+            }
+            Console.WriteLine(result);
+            return result;
+        }
+
+        private void btnEvaluate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string expression = textBoxExpression.Text.Trim();
+                if (expression == string.Empty)
+                {
+                    expression = NOExpression;
+                }
+                else
+                {
+                    expression = expression + " evaluates to " + Evaluate(expression);
+                }
+                UpdateResults(expression);
+            }
+            catch (Exception evaluateEx)
+            {
+                UpdateResults("Exception during expression evaluation: " + evaluateEx.Message);
+            }
+        }
+
+        // Design decisions - requirements stated the need of a simple expression evaluator so i kept it simple
+        // As software professionals we should add value, and enhance re-use and not re-write logic that already exists
+
+        //  Decided to avoid re-inventing the wheel
+        //  There are already dozens of 3rd party libraries available to support this requirement
+
+        // Generally - performance may be more critical but coders are expensive, so re-use of available tools 
+        //  is a valuable skill today - we are data processors and system integrators more and more these days.
+
+        // Extensibility - the Dataset.Compute() method is widely used and unlikely to go away any time soon, and already supports
+        //  more than the required capability defined in requirements.
+
+        // Trade-offs? Reduced delivery time now. Could multi-thread a middleware object to support many invokes per second later.
+        //   Reliant on Microsoft preserving support for Dataset.Compute() method - but we are always reliant on something in .Net :-)
+        
+    }
+}
